cd15eb1a731263e05a60095e9a16c9c6eb706c9d
Dylan Xu <dunyixu@gmail.com>
2022-07-23 21:10:19 +0800

    Add unittest

--- edrf_test.go
+++ edrf_test.go
@@ -4,177 +4,147 @@ import (
 	"testing"
 )
 
-type fakeCluster struct {
-	capacity Resources
-}
-
-func (c fakeCluster) Capacity() Resources {
-	return c.capacity
-}
-
-func (c fakeCluster) Binpack() bool {
-	return true
-}
-
-type fakeTask struct {
-	name   string
-	piece  Resources
-	limit  Resources
-	weight map[ResourceType]float64
-}
-
-func (t fakeTask) Name() string {
-	return t.name
-}
-
-func (t fakeTask) Piece() Resources {
-	return t.piece
-}
-
-func (t fakeTask) Limit(resourceType ResourceType) (ResourceAmount, bool) {
-	if t.limit == nil {
-		return 0, false
-	}
-	limit, ok := t.limit[resourceType]
-	return limit, ok
-}
-
-func (t fakeTask) Weight(resourceType ResourceType) (float64, bool) {
-	if t.weight == nil {
-		return 0.0, false
-	}
-	weight, ok := t.weight[resourceType]
-	return weight, ok
-}
-
 func TestDRF(t *testing.T) {
 	var tasks = []Task{
-		fakeTask{
-			name: "TaskA",
-			piece: Resources{
+		NewTask(
+			"A",
+			Resources{
 				ResourceCPU:    1,
 				ResourceMemory: 4,
 			},
-		},
-		fakeTask{
-			name: "TaskB",
-			piece: Resources{
+			nil,
+			nil,
+		),
+		NewTask(
+			"B",
+			Resources{
 				ResourceCPU:    3,
 				ResourceMemory: 1,
 			},
-		},
+			nil,
+			nil,
+		),
 	}
-	cluster := fakeCluster{
+	cluster := NewCluster(
 		Resources{
 			ResourceCPU:    9,
 			ResourceMemory: 18,
 		},
-	}
+		false,
+	)
+
 	e := New(cluster, tasks...)
-	assign := map[string]int{}
+	assignment := map[string]int{}
 	for {
 		task, err := e.Assign()
 		if err != nil {
 			break
 		}
-		assign[task.Name()]++
+		assignment[task.Name()]++
 	}
 
-	if cnt := assign["TaskA"]; cnt != 3 {
+	if cnt := assignment["A"]; cnt != 3 {
 		t.Errorf("TaskA: want 3 but got %d", cnt)
 	}
-	if cnt := assign["TaskB"]; cnt != 2 {
+	if cnt := assignment["B"]; cnt != 2 {
 		t.Errorf("TaskB: want 2 but got %d", cnt)
 	}
 }
 
 func TestEDRFWithLimit(t *testing.T) {
 	var tasks = []Task{
-		fakeTask{
-			name: "TaskA",
-			piece: Resources{
+		NewTask(
+			"A",
+			Resources{
 				ResourceCPU:    1,
 				ResourceMemory: 4,
 			},
-			limit: Resources{
+			Resources{
 				ResourceCPU: 2,
 			},
-		},
-		fakeTask{
-			name: "TaskB",
-			piece: Resources{
+			nil,
+		),
+		NewTask(
+			"B",
+			Resources{
 				ResourceCPU:    3,
 				ResourceMemory: 1,
 			},
-		},
+			nil,
+			nil,
+		),
 	}
-	cluster := fakeCluster{
+	cluster := NewCluster(
 		Resources{
 			ResourceCPU:    9,
 			ResourceMemory: 18,
 		},
-	}
+		false,
+	)
 	e := New(cluster, tasks...)
-	assign := map[string]int{}
+	assignment := map[string]int{}
 	for {
 		task, err := e.Assign()
 		if err != nil {
 			break
 		}
-		assign[task.Name()]++
+		assignment[task.Name()]++
 	}
 
-	if cnt := assign["TaskA"]; cnt != 2 {
+	if cnt := assignment["A"]; cnt != 2 {
 		t.Errorf("TaskA: want 3 but got %d", cnt)
 	}
-	if cnt := assign["TaskB"]; cnt != 2 {
+	if cnt := assignment["B"]; cnt != 2 {
 		t.Errorf("TaskB: want 2 but got %d", cnt)
 	}
 }
 
 func TestEDRFWithWeight(t *testing.T) {
 	var tasks = []Task{
-		fakeTask{
-			name: "TaskA",
-			piece: Resources{
+		NewTask(
+			"A",
+			Resources{
 				ResourceCPU:    1,
 				ResourceMemory: 2,
 			},
-			weight: map[ResourceType]float64{
+			nil,
+			map[ResourceType]float64{
 				ResourceCPU: 3.0,
 			},
-		},
-		fakeTask{
-			name: "TaskB",
-			piece: Resources{
+		),
+		NewTask(
+			"B",
+			Resources{
 				ResourceCPU:    2,
 				ResourceMemory: 1,
 			},
-			weight: map[ResourceType]float64{
+			nil,
+			map[ResourceType]float64{
 				ResourceCPU: 1.0,
 			},
-		},
+		),
 	}
-	cluster := fakeCluster{
+	cluster := NewCluster(
 		Resources{
 			ResourceCPU:    9,
 			ResourceMemory: 18,
 		},
-	}
+		false,
+	)
 	e := New(cluster, tasks...)
-	assign := map[string]int{}
+	assignment := map[string]int{}
 	for {
 		task, err := e.Assign()
 		if err != nil {
 			break
 		}
-		assign[task.Name()]++
+		assignment[task.Name()]++
 	}
 
-	if cnt := assign["TaskA"]; cnt != 5 {
+	if cnt := assignment["A"]; cnt != 5 {
 		t.Errorf("TaskA: want 5 but got %d", cnt)
 	}
-	if cnt := assign["TaskB"]; cnt != 2 {
+	if cnt := assignment["B"]; cnt != 2 {
 		t.Errorf("TaskB: want 2 but got %d", cnt)
 	}
 	t.Log(e.Describe())
